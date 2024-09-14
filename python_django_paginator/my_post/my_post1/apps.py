66466576a58e8e4b344908688f73d1406761c2a8
Дмитрий <160622322+Only-Clean-Cat@users.noreply.github.com>
2024-09-14 20:54:22 +0300

    Домашнее задание по теме "Пагинация"

---
+++ python_django_paginator/my_post/my_post1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyPost1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_post1'
---
+++ python_django_paginator/my_post/my_post1/views.py
@@ -0,0 +1,18 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+def index_post(request):
+    items_per_page = request.GET.get('items_per_page', 1)
+    items = Post.objects.all().order_by('-created_id')
+    paginator = Paginator(items, items_per_page)
+    # posts = Post.objects.all().order_by('-created_id')
+    # paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    print(items_per_page)
+    # print(page_number)
+    # print(page_obj)
+    return render(request, 'index_post.html', {'page_obj': page_obj})
